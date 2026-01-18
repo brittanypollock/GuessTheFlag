030d29bbf1680e67a1fd7cce40a1c3505bf77151
Brittany Pollock <brittpollock15@gmail.com>
2026-01-18 13:03:47 -0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Brittany Pollock on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
